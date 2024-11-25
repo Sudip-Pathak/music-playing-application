aa4fbb93ae41ba52765d5bcb49af0f681c5b29a9
Sudip Pathak <sudippathak10000@gmail.com>
2024-11-25 22:09:32 +0545

    Continuing the work of the previous commit as well as working with Songitem, DisplayAlbum and Albumitem created in the components.

---
+++ src/components/DisplayAlbum.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { useParams } from "react-router";
+import { albumsData, assets } from "../assets/assets";
+
+const DisplayAlbum = () => {
+  const { id } = useParams();
+  const albumData = albumsData[id];
+
+  return (
+    <>
+      <Navbar />
+      <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-end">
+        <img className="w-48 rounded" src={albumData.image} alt="" />
+        <div className="flex flex-col">
+          <p>Playlist</p>
+          <h2 className="text-5xl font-bold mb-4 md:text-7xl">
+            {albumData.name}
+          </h2>
+          <h4>{albumData.desc}</h4>
+          <p className="mt-1">
+            <img
+              className="inline-block w-5"
+              src={assets.spotify_logo}
+              alt=""
+            />
+            <b> Neplify </b>
+            • 1,20,00,000 Likes • <b>50 Songs, </b>
+            about 2hrs 30min.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayAlbum;
