46fc48fc7ad5017af394415e75039de73ce61e30
Sudip Pathak <sudippathak10000@gmail.com>
2024-12-04 22:05:51 +0545

    Working with addong in the songController

--- backend/src/routes/songRoute.js
+++ backend/src/routes/songRoute.js
@@ -1,11 +1,12 @@
 import { addSong, listSong } from "../controllers/songcontroller.js";
 import express from "express";
+import upload from "../middleware/multer.js";
 
 const songRouter = express.Router();
 
 // Using this songRouter router we can create multiple apis.
 
-songRouter.post("/add", addSong);
+songRouter.post("/add", upload.fields([{name:'image', maxCount:1}, {name:'audio', maxCount:1}]), addSong);
 songRouter.get("/list", listSong);
 
 export default songRouter;
