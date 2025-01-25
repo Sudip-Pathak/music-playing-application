4e2eb42b4263c3ff2ff5e7fed7fd3b3dabc4a1cf
Sudip Pathak <sudippathak10000@gmail.com>
2025-01-25 09:49:51 +0545

    Continuation of the project by changing the cloudinary and MongoDB login status

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,10 +1,22 @@
-import { v2 as clodinary } from "cloudinary";
+// import { v2 as clodinary } from "cloudinary";
+
+// const connectCloudinary = async () => {
+//   await clodinary.config({
+//     cloud_name: process.env.CLOUDINARY_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_KEY,
+//   });
+// };
+
+// export default connectCloudinary;
+
+import { v2 as cloudinary } from "cloudinary";
 
 const connectCloudinary = async () => {
-  await clodinary.config({
+  await cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY, // Corrected this line
   });
 };
 
