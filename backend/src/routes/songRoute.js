102aef2e009a8dafc70c179122e078ec7fbd958f
Sudip Pathak <sudippathak10000@gmail.com>
2025-01-26 15:35:34 +0545

    Successfull in file uploading using multer, post method successful.

--- backend/src/routes/songRoute.js
+++ backend/src/routes/songRoute.js
@@ -6,10 +6,9 @@ import upload from "../middleware/multer.js"; // assuming this is your multer co
 const songRouter = express.Router();
 
 // Setup the multer fields handling correctly
-const uploadFields = multer().fields([
+const uploadFields = upload.fields([
   { name: "image", maxCount: 1 },
   { name: "audio", maxCount: 1 },
-  upload,
 ]);
 
 songRouter.post("/add", uploadFields, addSong);
