be9e8e71c35a19b5078ed5270549c5aaace794a1
Sudip Pathak <sudippathak10000@gmail.com>
2025-01-29 19:34:47 +0545

    Album Controller code fro adding the album

--- backend/src/controllers/albumController.js
+++ backend/src/controllers/albumController.js
@@ -3,7 +3,31 @@
 import { v2 as cloudinary } from "cloudinary";
 import albumModel from "../models/albumModel.js";
 
-const addAlbum = async (req, res) => {};
+// Controller for adding Album.
+const addAlbum = async (req, res) => {
+  try {
+    const name = req.body.name;
+    const desc = req.body.desc;
+    const bgColour = req.body.bgColour;
+    const imageFile = req.file;
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const albumData = {
+      name,
+      desc,
+      bgColour,
+      image: imageUpload.secure_url,
+    };
+
+    const album = albumModel(albumData);
+    await album.save();
+
+    res.json({ suceess: true, messge: "Album Added" });
+  } catch {
+    res.json({ suceess: false, message: "Song Not Added" });
+  }
+};
 
 const listAlbum = async (req, resp) => {};
 
