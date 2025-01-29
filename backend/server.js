6d671bcf8b4b91590e2f0ecf2c854e849c5b5c48
Sudip Pathak <sudippathak10000@gmail.com>
2025-01-29 18:58:25 +0545

    Setting the route for the album in albumRoute.js and server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import songRouter from "./src/routes/songRoute.js";
 import connectDB from "./src/config/mongodb.js";
 import connectCloudinary from "./src/config/cloudinary.js";
+import albumRouter from "./src/routes/albumRoute.js";
 
 // app config especially database connection.
 const app = express();
@@ -16,6 +17,7 @@ app.use(express.json()); // after getting any request, this json parse that requ
 app.use(cors()); // this method connects frontend to the bacckend.
 
 // initializing routes
-app.use("/api/song", songRouter); // if we need the list of all songs, we go through this api path
+app.use("/api/song", songRouter); // base route for songRoute.js
+app.use("/api/album", albumRouter); // base route fro albumRoute.js
 app.get("/", (req, res) => res.send("API Working"));
 app.listen(port, () => console.log(`Server is runing on ${port}`));
--- backend/src/controllers/albumController.js
+++ backend/src/controllers/albumController.js
@@ -1,2 +1,12 @@
 // Working with album controller.
 
+import { v2 as cloudinary } from "cloudinary";
+import albumModel from "../models/albumModel.js";
+
+const addAlbum = async (req, res) => {};
+
+const listAlbum = async (req, resp) => {};
+
+const removeAlbum = async (req, res) => {};
+
+export { addAlbum, listAlbum, removeAlbum };
