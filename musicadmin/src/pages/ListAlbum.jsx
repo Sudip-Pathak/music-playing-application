5f75f0abc9e4cd7e9b0a277c6b0c79ff932fd3ce
Sudip Pathak <sudippathak10000@gmail.com>
2025-02-03 14:55:01 +0545

    Completed remove Album functionality, connecting the backend and frontend in ListAlbum.jsx

--- musicadmin/src/pages/ListAlbum.jsx
+++ musicadmin/src/pages/ListAlbum.jsx
@@ -1,57 +1,3 @@
-// import axios from "axios";
-// import React, { useEffect, useState } from "react";
-// import { url } from "../App";
-// import { toast } from "react-toastify";
-
-// const ListAlbum = () => {
-//   const [data, setData] = useState([]);
-
-//   const fetchAlbums = async () => {
-//     try {
-//       const response = await axios.get(`${url}/api/album/list`);
-
-//       if (response.data.success) {
-//         setData(response.data.albums);
-//       }
-//     } catch (error) {
-//       toast.error("Error Occured While listing Album");
-//     }
-//   };
-//   // To execute the function whenever components get loaded, useEffect hook
-//   useEffect(() => {
-//     fetchAlbums;
-//   });
-
-//   return (
-//     <div>
-//       <p>List of All Albums</p>
-//       <br />
-//       <div>
-//         <div className="sm:grid hiden grid-cols-[0.5f_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5 bg-gray-100">
-//           <b>Image</b>
-//           <b>Name</b>
-//           <b>Description</b>
-//           <b>Album Colour</b>
-//           <b>Action</b>
-//         </div>
-//               {data.map((item, index) => {
-//                   return (
-//                       <div key={index} className="grid grid-cols-[1fr_1fr_1fr] sm:grid-cols-[0.5fr_1fr_2fr_1fr_o.5fr] items-center gap-2.5 p-3 border-gray-300 text-sm mr-5">
-//                           <img className="w-12" src={item.image} alt="" />
-//                           <p>{item.name}</p>
-//                           <p>{item.desc}</p>
-//                           <input type="color" value={item.bgColour} />
-//                           <p className="cursor-pointer">Delete</p>
-//                           <div />
-//             )
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ListAlbum;
-
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { url } from "../App";
@@ -69,6 +15,18 @@ const ListAlbum = () => {
       }
     } catch (error) {
       toast.error("Error Occurred While listing Album");
+    }
+  };
+
+  const removeAlbum = async (id) => {
+    try {
+      const response = await axios.post(`${url}/api/album/remove`, { id });
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchAlbums();
+      }
+    } catch (error) {
+      toast.error("Error Occurred While Removing Album");
     }
   };
 
@@ -99,7 +57,12 @@ const ListAlbum = () => {
               <p>{item.name}</p>
               <p>{item.desc}</p>
               <input type="color" value={item.bgColour} />
-              <p className="cursor-pointer">Delete</p>
+              <p
+                onClick={() => removeAlbum(item._id)}
+                className="cursor-pointer"
+              >
+                Delete
+              </p>
             </div>
           );
         })}
