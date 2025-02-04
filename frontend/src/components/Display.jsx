2c3a10be96fed1a868af0c9149828a19cb1e0a19
Sudip Pathak <sudippathak10000@gmail.com>
2025-02-04 18:56:57 +0545

    Allmost basic part of the code is complete. Now the advance part of the code is left.

--- frontend/src/components/Display.jsx
+++ frontend/src/components/Display.jsx
@@ -77,10 +77,12 @@ const Display = () => {
       ref={displayRef}
       className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
     >
-      <Routes>
-        <Route path="/" element={<DisplayHome />} />
-        <Route path="/album/:id" element={<DisplayAlbum album={album} />} />
-      </Routes>
+      {albumsData.length > 0 ? (
+        <Routes>
+          <Route path="/" element={<DisplayHome />} />
+          <Route path="/album/:id" element={<DisplayAlbum album={album} />} />
+        </Routes>
+      ) : null}
     </div>
   );
 };
